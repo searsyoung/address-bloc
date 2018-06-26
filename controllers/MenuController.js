2075553d2015061b9657a1bb42732fceba6aa98e
searsyoung <sears.young@gmail.com>
2018-06-26 09:38:22 -0700

    Checkpoint complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const ContactController = require("./ContactController");
 
 module.exports = class MenuController {
     constructor() {
@@ -15,7 +16,7 @@ module.exports = class MenuController {
                 ]
             }
         ];
-        this.contacts = [];
+        this.book = new ContactController();
     }
 
     main() {
@@ -48,18 +49,21 @@ module.exports = class MenuController {
     }
 
     addContact() {
-        this.clear();
-        console.log('addContact called');
-        this.main();
+        inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+            this.book.addContact(answers.name, answers.phone).then((contact) => {
+                console.log("Contact added successfully!");
+                this.main();
+            }).catch((err) => {
+                console.log(err);
+                this.main();
+            });
+        });
     }
     getDate() {
         let today = new Date().toJSON();
         this.clear();
         console.log("The current date and time is: " + today)
         this.main()
-    }
-    getContactCount() {
-        return this.contacts.length;
     }
 
     remindMe() {
